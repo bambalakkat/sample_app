27322c648adbf5e1c0049de8a8fcfbebae0a2363
Balagopal Ambalakkat <bambalakkat@gmail.com>
2011-07-31 12:54:28 -0700

    user model validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,10 @@
 class User < ActiveRecord::Base
-  attr_accessible: :name, :email
+  attr_accessible :name, :email
+  validates :name, :presence => true,
+                   :length => {:maximum => 50 }
+                   
+  validates :email, :presence => true,
+                    :uniqueness => {:case_sensitive => false},
+                    :format => { :with => /^([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})$/i } 
+  
 end
