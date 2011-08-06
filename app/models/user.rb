3c7ed62a0791c9a28123f191358e53d0c7f8c158
Balagopal Ambalakkat <bambalakkat@gmail.com>
2011-08-06 10:26:40 -0700

    Added password and encrypted password attributes to the user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,15 @@
 class User < ActiveRecord::Base
-  attr_accessible :name, :email
+  attr_accessor   :password
+  attr_accessible :name, :email, :password, :password_confirmation
   validates :name, :presence => true,
                    :length => {:maximum => 50 }
                    
   validates :email, :presence => true,
                     :uniqueness => {:case_sensitive => false},
                     :format => { :with => /^([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})$/i } 
+                    
+  validates :password, :presence => true,
+                       :confirmation => true,
+                       :length => { :within => 6..40 }
   
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,19 +1,25 @@
 require 'spec_helper'
 
+
 describe User do
 
   before(:each) do
-    @attr = { :name => 'Example User', :email => 'exampleuser@example.com'}
+    @attr = { 
+              :name => 'Example User', 
+              :email => 'exampleuser@example.com',
+              :password => 'foobar',
+              :password_confirmation => 'foobar'
+            }
   end
 
   it  "should create a new instance given valid attribute" do
     User.create!(@attr)
   end
-  
+
   it "should require a name" do
     no_name_user = User.new(@attr.merge(:name => ""))
     no_name_user.should_not be_valid
-  end 
+  end
   it "should require an email address" do
     no_name_user = User.new(@attr.merge(:email => ""))
     no_name_user.should_not be_valid
@@ -22,8 +28,8 @@ describe User do
     long_name = "a" * 51
     no_name_user = User.new(@attr.merge(:name => long_name))
     no_name_user.should_not be_valid
-  end 
-  
+  end
+
   it "should accept valid email adresses" do
     addresses = %w[user@foo.com THE_USER@foo.bar.org first.last@foo.jp]
     addresses.each do |address|
@@ -31,7 +37,7 @@ describe User do
       valid_email_user.should be_valid
     end
   end
-  
+
   it "should reject invalid email adresses" do
     addresses = %w[user.foo.com THE_USER@org. first.last_foo,jp]
     addresses.each do |address|
@@ -43,16 +49,64 @@ describe User do
     User.create!(@attr)
     user_with_duplicate_email = User.new(@attr)
     user_with_duplicate_email.should_not be_valid
-    
-  end 
-  
+
+  end
+
   it "should reject duplicate email adresses identical up to case" do
     upcased_email = @attr[:email].upcase
     User.create(@attr.merge(:email => upcased_email))
     user_with_duplicate_email =  User.new(@attr)
-     user_with_duplicate_email.should_not be_valid
+    user_with_duplicate_email.should_not be_valid
 
-   end 
+  end
+
+  describe "passwords" do
+
+    before(:each) do
+      @user = User.new(@attr)
+    end
+
+    it "should have a password attribute" do
+       @user.should respond_to(:password)
+    end
+
+    it "should have password confirmation attribute" do
+       @user.should respond_to(:password_confirmation)
+    end
+  end
   
+  describe "password validations" do
+    
+    it "should require a password" do
+      User.new(@attr.merge(:password => "", :password_confirmation => "")).should_not be_valid
+    end
+    
+    it "should require a matching password confirmation" do
+      User.new(@attr.merge(:password_confirmation => "invalid")).should_not be_valid
+    end
+    
+    it "should reject short passwords" do
+      short = "a" * 5
+      hash = @attr.merge(:password => short, :password_confirmation => short)
+      User.new(hash).should_not be_valid
+    end
+    it "should reject long passwords" do
+      long = "a" * 41
+      hash = @attr.merge(:password => long, :password_confirmation => long)
+      User.new(hash).should_not be_valid
+    end  
+  end
   
+  describe "password encryption" do
+    
+    before(:each) do
+      @user = User.create!(@attr)
+    end
+    
+    it "should have an encrypted password" do
+      @user.should respond_to(:encrypted_password)
+    end
+    
+  end
+
 end
