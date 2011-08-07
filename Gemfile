5983989ef9aa09b7c61478af8adbd4f36405f84f
Balagopal Ambalakkat <bambalakkat@gmail.com>
2011-08-06 17:34:43 -0700

    added user factory and test for user show

--- Gemfile
+++ Gemfile
@@ -33,4 +33,5 @@ end
 group :test do
    gem 'rspec-rails', '2.6.1'
    gem 'webrat'
+   gem 'factory_girl_rails'
 end
---
+++ spec/factories.rb
@@ -0,0 +1,6 @@
+Factory.define :user do |user|
+  user.name                  "Balagopal Ambalakkat"
+  user.email                 "amb@example.com"
+  user.password              "foobar"
+  user.password_confirmation "foobar"
+end
